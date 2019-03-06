5c4c8b85724f52c43ae125372718fe396fc9136e
Jeremy Lee <jlee00@stuy.edu>
2019-03-06 14:46:14 -0500

    Initializing pasture almost complete

--- USACO.java
+++ USACO.java
@@ -16,9 +16,29 @@ public class USACO{
       finalElevation = Integer.parseInt(inf.next());
       command = Integer.parseInt(inf.next());
       pasture = new int[rows][cols];
+      int bigCount = 0;
       while(inf.hasNextLine()){
           String line = inf.nextLine();
-          System.out.println(line);
+          if (line.length() == (cols*2) + (cols-1)){
+            String currentNum = "";
+            int count = 0;
+            for (int i=0; i<line.length(); i++){
+              if (line.charAt(i) == ' '){
+                pasture[bigCount][count] = Integer.parseInt(currentNum);
+                count++;
+                currentNum = "";
+              } else {
+                currentNum += line.charAt(i);
+              }
+            }
+            bigCount++;
+          }
+      }
+      for (int row=0; row<pasture.length; row++){
+        for (int col=0; col<pasture[row].length; col++){
+          System.out.print(pasture[row][col] + " ");
+        }
+        System.out.println();
       }
     } catch (FileNotFoundException e){
       System.out.println("File not found");
@@ -29,4 +49,6 @@ public class USACO{
   public static int silver(String filename){
     return 0;
   }
+
+
 }

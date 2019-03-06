22fd6988044577c6b372a6cd6575618bc1046f67
Jeremy Lee <jlee00@stuy.edu>
2019-03-06 14:34:44 -0500

    Added file reader

--- USACO.java
+++ USACO.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class USACO{
   public static int bronze(String filename){
+    int[][] pasture;
+    int rows;
+    int cols;
+    int finalElevation;
+    int command;
+    try {
+      File text = new File(filename);
+      Scanner inf = new Scanner(text);
+      rows = Integer.parseInt(inf.next());
+      cols = Integer.parseInt(inf.next());
+      finalElevation = Integer.parseInt(inf.next());
+      command = Integer.parseInt(inf.next());
+      pasture = new int[rows][cols];
+      while(inf.hasNextLine()){
+          String line = inf.nextLine();
+          System.out.println(line);
+      }
+    } catch (FileNotFoundException e){
+      System.out.println("File not found");
+    }
     return 0;
   }
 

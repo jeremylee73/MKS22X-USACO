4765c236121fbef30ef30423bb54f9a7357a57fe
Jeremy Lee <jlee00@stuy.edu>
2019-03-10 21:48:11 -0400

    Passes sample test

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
   public static void main(String[] args){
     System.out.println(USACO.bronze("sample.txt"));
-    USACO.silver("silver_sample.txt");
+    System.out.println(USACO.silver("silver_sample.txt"));
   }
 }
